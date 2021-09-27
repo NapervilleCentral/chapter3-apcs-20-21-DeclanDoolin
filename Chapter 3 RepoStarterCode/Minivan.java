494f882f18c58639c66bd6ca1caed9e2e4e93900
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-09-27 12:54:19 -0500

    Started MiniVan

--- Chapter 3 RepoStarterCode/Minivan.java
+++ Chapter 3 RepoStarterCode/Minivan.java
@@ -5,13 +5,40 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.*;
 public class Minivan
 {
     public static void main(String[] args)
     {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the sequence: ");
+        String input = keyboard.nextLine();
+        boolean right = false;
+        boolean left = false;
+        
+        if (input.charAt(3) == 1 && input.charAt(8) == 'P'){
+            if (input.charAt(1) == 1){
+                right = true;
+            }
+            if (input.charAt(0) == 1){
+                left = true;
+            }
+        }    
+        else{
+            System.out.print("Doors cannot open, car is not in park or master lock is on");
+        }
+     
+            
+            
+            
+            
+            
+            
+    }       
+            
+            
+}            
+            
+               
+        
     
-       
-        
-        
-    }
-}
