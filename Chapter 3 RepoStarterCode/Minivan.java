93881058c58947c1eb67222908ce748b586e9ca9
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-09-29 17:57:38 -0500

    Worked on minivan

--- Chapter 3 RepoStarterCode/Minivan.java
+++ Chapter 3 RepoStarterCode/Minivan.java
@@ -16,18 +16,32 @@ public class Minivan
         String right = "Closed";
         String left = "Closed";
         
-        if (input.charAt(3) == 1 && input.charAt(8) == 'P'){
+        if (input.charAt(3) == 1 && input.charAt(8) == 'P') {      
             if (input.charAt(0) == 1){
-                left = "Open";
+                 left = "Opens";
             }
-            if (input.charAt(1) == 1){
-                right = "Open";
+            else if (input.charAt(1) == 1){
+                right = "Opens";
             }
-            if (input.charAt(2)
+            else if (input.charAt(2) == 0){
+                if (input.charAt(4) == 1)
+                    left = "Opens";
+                if (input.charAt(5) == 1)
+                    right = "Opens";
+            }
+            else if (input.charAt(6) == 1){
+                left = "Opens";
+            }
+            else if (input.charAt(7) == 1){
+                left = "Opens";
+            } 
+            System.out.println("The left door "+left);
+            System.out.println("The right door "+right );
         }    
         else{
-            System.out.print("Doors cannot open, car is not in park or master lock is on");
+            System.out.print("Both doors stay closed");
         }
+        
      
             
             
