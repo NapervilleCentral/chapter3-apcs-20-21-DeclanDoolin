1229350fc1f2f8c1ce03fba54c4f17978167e4fe
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-09-30 14:13:34 -0500

    Finished minivan, started taxes

--- Chapter 3 RepoStarterCode/Taxes.java
+++ Chapter 3 RepoStarterCode/Taxes.java
@@ -12,26 +12,87 @@ public class Taxes
         Scanner in = new Scanner(System.in);
         System.out.println("What is your yearly income?");
         double income = in.nextDouble();
-        double tax = 0.00;
+        double tax = 0.0;
+        double tax1 = 0.0;
+        double tax2 = 0.0;
+        double newincome = 0.0;
         
         //Creates if statement, calculates the tax depending on level of income
-        if (income <= 50000.00){
+        if (income < 50000.0){            
             tax = (income*.01);
         }
-        else if (income >= 50000.00 && income <= 75000.00){
-            tax = (income*.02);
+        else if (income >= 50000.0 && income < 75000.0){
+            newincome = (income - 50000.0);
+            tax1 = (newincome*.02);
+            tax2 = (income *.01);
+            tax = (tax1+tax2);
         }      
-        else if (income >= 75000.00 && income <= 100000.00){
-            tax = (income*.03);
+        else if (income >= 75000.0 && income < 100000.0){
+            newincome = (income - 75000.0);
+            tax1 = (newincome*.03);
+            tax2 = (income *.01);
+            tax = (tax1+tax2);
         }
-        else if (income >= 100000.00 && income <= 250000.00){
-            tax = (income*.04);
+        else if (income >= 100000.0 && income < 250000.0){
+            newincome = (income-100000.0);
+            tax1 = (newincome*.04);
+            if (newincome >= 100000.0){
+                tax2 = (income*.04);
+            }
+            if (newincome >= 75000.0){
+                tax2 = (income *.03);
+            }
+            if (newincome>= 50000.0){
+                tax2 = (income *.02);
+            }
+            else{
+                tax2 = (income *.01);
+            }
+            tax = (tax1+tax2);
         }
-        else if (income >= 250000.00 && income <= 500000.00){
-            tax = (income*.05);
+        else if (income >= 250000.0 && income < 500000.0){ 
+            newincome = (income-250000.0);
+            tax1 = (newincome*.05);            
+            if (newincome >= 100000.0){
+                tax2 = (income*.04);
+            }
+            if (newincome >= 75000.0){
+                tax2 = (income *.03);
+            }
+            if (newincome>= 50000.0){
+                tax2 = (income *.02);
+            }
+            else{
+                tax2 = (income *.01);
+            }
+            tax = (tax1+tax2);
         }
-        else
-            tax = (income*.06);
+        else{
+            newincome = (income-500000.0);
+            tax1 = (newincome*.06);
+            if (newincome >= 500000.0){
+                tax2 = (income *.06);
+            }
+            if (newincome >= 250000.0){
+                tax2 = (income*.05);
+            }
+            if (newincome >= 100000.0){
+                tax2 = (income*.04);
+            }
+            if (newincome >= 75000.0){
+                tax2 = (income *.03);
+            }
+            if (newincome>= 50000.0){
+                tax2 = (income *.02);
+            }
+            else{
+                tax2 = (income *.01);
+            }
+            tax = (tax1+tax2);
+        }
+        
+        
+            
         //Prints yearly income, and corresponding tax
         System.out.println("Income of "+income);
         System.out.println("Tax:" +tax);
