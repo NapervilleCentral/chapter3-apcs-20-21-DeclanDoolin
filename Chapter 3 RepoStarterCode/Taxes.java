9186897131eaff5d6eb5a18cac91af0c9311bb20
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-09-30 18:27:16 -0500

    Finished Tax

--- Chapter 3 RepoStarterCode/Taxes.java
+++ Chapter 3 RepoStarterCode/Taxes.java
@@ -15,23 +15,25 @@ public class Taxes
         double tax = 0.00;
         
         //Creates if statement, calculates the tax depending on level of income
-        if (income <= 50000.00){
+        if (income < 50000.00){
             tax = (income*.01);
         }
-        else if (income >= 50000.00 && income <= 75000.00){
-            tax = (income*.02);
+        if (income >= 50000.00){
+            tax = ((income-50000.00)*.02) + (50000.00 * .01);
         }      
-        else if (income >= 75000.00 && income <= 100000.00){
-            tax = (income*.03);
+        if (income >= 75000.00){
+            tax = ((income-75000.00)*.03) + (25000.00 * .02) + (50000.00 * .01);
         }
-        else if (income >= 100000.00 && income <= 250000.00){
-            tax = (income*.04);
+        if (income >= 100000.00){
+            tax = ((income-100000.00)*.04) + (25000.00 * .03) + (25000.00 *.02) + (50000.00 * .01);
         }
-        else if (income >= 250000.00 && income <= 500000.00){
-            tax = (income*.05);
+        if (income >= 250000.00){
+            tax = ((income-250000.00)*.05) + (150000.00 * .04) + (25000.00 * .03) + (25000.00 *.02) + (50000.00 *.01);
         }
-        else
-            tax = (income*.06);
+        if (income >= 500000.00){
+            tax = ((income-500000.00) * .06) + (250000.00 * .05) + (150000.00 * .04) + (25000.00 * .03) + (25000.00 * .02) + (50000.00 *.01);
+        }
+        
         //Prints yearly income, and corresponding tax
         System.out.println("Income of "+income);
         System.out.println("Tax:" +tax);
