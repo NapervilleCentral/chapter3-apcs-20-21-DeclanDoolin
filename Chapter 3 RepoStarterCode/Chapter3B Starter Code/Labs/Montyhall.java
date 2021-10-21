8c2065e7ce6db959d480be4b5c8c3c162a38af28
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-10-21 14:13:26 -0500

    Started monty hall

---
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/Montyhall.java
@@ -0,0 +1,20 @@
+package Labs;
+
+
+/**
+ * Write a description of class Montyhall here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class Montyhall
+{
+    public static void main(String[] args){
+        int s1 = 0;
+        int s2 = 0;
+        
+        
+        
+    }
+}
--- Chapter 3 RepoStarterCode/Chapter3B Starter Code/e22/MontyHall.java
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/e22/MontyHall.java
@@ -1,3 +1,5 @@
+package e22;
+
 import java.util.Random;
 
 public class MontyHall
