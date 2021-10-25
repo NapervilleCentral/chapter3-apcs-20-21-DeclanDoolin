969d7085e1e116c0741b05433b00448f7d636e90
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-10-25 14:13:31 -0500

    Worked on palidrome and spiral

--- Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/Palindrome.java
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/Palindrome.java
@@ -15,7 +15,26 @@ public class Palindrome
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter a string: ");
         String input = in.nextLine();
+        int length = input.length();        
+        String normal = "";
+        String reversed = "";
         if (input != ""){
+            for (int i = 0; i < (length+1); i++){
+                if (input.charAt(length-1) == i)
+                    reversed = "yes"; 
+            if (reversed == "yes")
+                System.out.println("Palindrome!");
+                   
+                
+                
+                 
+                
+                
+                
+            }
+                
+            
+             
             
                 
                 
---
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/Spiral.java
@@ -0,0 +1,21 @@
+package Labs;
+
+
+/**
+ * Write a description of class Spiral here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import TurtleGraphics.StandardPen;
+public class Spiral
+{
+    public static void main(String[] args){
+        StandardPen pen = new StandardPen();
+        for (int i = 0; i < 100; i++){
+            
+        }
+        
+    }
+}
