0933de0f88d39a0fd37132b5cfeae1e9feccf6f0
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-10-26 21:30:10 -0500

    Finished 4 leaf rose

--- Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/FourLeafRose.java
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/FourLeafRose.java
@@ -15,9 +15,21 @@ public class FourLeafRose
     public static void main(String[] args)
     {
         StandardPen pen = new StandardPen();
-        double r = .99;
-        double pheta = 6.28;
-        double x = r * .99;
+        int maximum = 100;
+        for (int i = 0; i <= 100; i++){
+            double theta = i * 2 * Math.PI / maximum;
+            double r = Math.cos(2*theta);
+            double x = 100 * r * Math.cos(theta) + 200;
+            double y = 100 * r * Math.sin(theta) + 200;
+            if (i == 0){
+                pen.move(x,y);       
+            }
+            else{
+                pen.move(x,y);
+            }
+            }
+        }
+        
         
     }
-}
+
