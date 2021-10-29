bc11e340ac31aae940fa5460122e5680447930e4
DeclanDoolin <djdoolin@stu.naperville203.org>
2021-10-29 14:13:49 -0500

    worked on mustery

--- Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/StringMystery.java
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/Labs/StringMystery.java
@@ -7,9 +7,38 @@ package Labs;
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.*;
+import java.io.*;
 public class StringMystery
 {
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException{
+        Scanner mys = new Scanner (new File("labs/String Mystery input.txt"));
+        String file = mys.nextLine();
+        int j = file.length()-1;
+        int start = Integer.parseInt(file.substring(0, 1));
+        int finalnum = Integer.parseInt(file.substring(2195, 2196));
+        int totalval = 0;
+        int i = 0;
+        int present,second;
+        if (finalnum == start){
+            totalval += finalnum;
+        }
+        while (mys.hasNext()){
+            for(i=0;i<file.length()-1;i++)
+                present = Integer.parseInt(file.substring(i, i+1));
+                second = Integer.parseInt(file.substring(i+1, i+2));
+                if (present == second)
+                    totalval += second;
+            
+        }
+        System.out.println(totalval);    
+            
+            
+            
+        }
+        
+        
+        
         
     }
-}
+
